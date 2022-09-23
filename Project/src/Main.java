5a02239f6c47e081c2dddcd653fd309213e3522a
ahfei <ahfei@172.28.167.208>
2022-09-23 15:42:11 +0800

    Fei is here again

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Sam said,'I am a boss!'");
-		System.out.println("Fei's here!");
+		System.out.println("Fei's here!!!!");
 		System.out.println("Lok's here!");
 		System.out.println("Elly's here!");
 		System.out.println("Yoyo's here!");

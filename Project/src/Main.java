0aae3e915668494fb61d8f7a781061ba02873b31
samng <samng@localhost>
2022-09-23 15:39:40 +0800

    I'm not that arrogant ar

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,7 +1,6 @@
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Sam said,'I am a boss!'");
+		System.out.println("Sam's here");
 		System.out.println("Fei's here!");
 		System.out.println("Lok's here!");
 		System.out.println("Elly's here!");

cb1979bab03a95204a0b3a052fe1d03dcba77672
USER <USER@192.168.21.1>
2022-09-23 15:13:20 +0800

    20220923 Lok first commit

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Sam's here!");
 		System.out.println("Fei's here!");
-
+		System.out.println("Lok's here!");
     }
 }

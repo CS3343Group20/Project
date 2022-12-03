c8c559066506cf8bb9de9603001b634be12d556b
s710643205 <wong.ken1224@gmail.com>
2022-12-04 02:25:23 +0800

    Updated a lot

--- src/main/FileReader.java
+++ src/main/FileReader.java
@@ -5,7 +5,9 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import controlSystem.CMS;
 import controlSystem.Request;
+import exceptions.FloorRangeException;
 import exceptions.InsufficientArgumentException;
 import time.TimeConverter;
 import utils.RequestComparator;
@@ -34,6 +36,9 @@ public class FileReader {
         		parseInTime=TimeConverter.ConvertTime(requestTime);	
 	            int currentFloor = Integer.parseInt(inputcmd[1]);
 	            int targetFloor = Integer.parseInt(inputcmd[2]);
+				if (currentFloor > CMS.getInstance().getBuilding().getFloorCount() ||targetFloor > CMS.getInstance().getBuilding().getFloorCount()||currentFloor < 0||targetFloor < 0) {        		
+	        		throw new FloorRangeException();
+	        	}
 	            int weight = Integer.parseInt(inputcmd[3]);
 	            Passenger p=new Passenger(weight,currentFloor,targetFloor);
 	            inputList.add(new Request(p,parseInTime));
--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -15,44 +15,37 @@ public class Simulator {//userless now just ignore it
 	public Simulator() {
 		currentTime=0;
 	}
-	public void StartSimulation(Building b, ArrayList<Request> inputList) {
-		System.out.printf("------------------Start simulation-----------------%n%n");
+	public void StartSimulation(Building building, ArrayList<Request> inputList) {
+		System.out.printf("------------------Start simulation!-----------------%n%n");
 		while (currentTime!=86400) {
 			Iterator<Request> itr=inputList.iterator();
 			putRequest(itr);
-//			while (itr.hasNext()) {
-//				Request r=itr.next();
-//				if (r.getRequestTime()<=currentTime) {//put request inside the req System when time meet in input
-//					r.getPassenger().makeRequest(currentTime);
-//					itr.remove();
-//				}
-//				else break;
-//			}
 			cms.setCurrentTime(currentTime);
 			if(!cms.curHaveRequest()) {
 				if(cms.anyLiftRunning()) {
 					cms.operate(currentTime);
 				}
 			}
-			else {//{cur bug: if have multiple ppl on same req flr & dir flr, will only handle the first ppl}
-				for (Map.Entry<Integer, Floor> flrMap: b.getFlrMap().entrySet()) {//check all req in all floor
-					Floor f=flrMap.getValue();
-					if (f.haveUpReq(currentTime)&&!f.getUpflag()) {//have request and not yet accepted by any lift
-						cms.assignClosest(flrMap.getKey(),1);
-						System.out.println("trigger up assign");
-					}
-					if (f.haveDownReq(currentTime)&&!f.getDownflag()) {
-						cms.assignClosest(flrMap.getKey(),0);
-						System.out.println("trigger down assign");
-					}
-				}
+			else {
+				assignLift(building);
 				cms.operate(currentTime);
 			}
 			currentTime++;
 		}
-		System.out.println("Simulation ends!");
+		System.out.println("------------------Simulation ends!------------------");
 	}
 	
+	private void assignLift(Building building) {
+		for (Map.Entry<Integer, Floor> flrMap: building.getFlrMap().entrySet()) {//check all req in all floor
+			Floor f=flrMap.getValue();
+			if (f.haveUpReq(currentTime)&&!f.getUpflag()) {//have request and not yet accepted by any lift
+				cms.assignClosest(flrMap.getKey(),1);
+			}
+			if (f.haveDownReq(currentTime)&&!f.getDownflag()) {
+				cms.assignClosest(flrMap.getKey(),0);
+			}
+		}
+	}
 	public void putRequest(Iterator<Request> itr) {
 		while (itr.hasNext()) {
 			Request r=itr.next();
@@ -62,6 +55,7 @@ public class Simulator {//userless now just ignore it
 			}
 			else break;
 		}
-		
 	}
+
+
 }
